08c70f376d7870275dffca9fd0d37d0ee95bc333
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-09 13:08:55 +0100

    rspec files created

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,4 +97,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+onfig.color = true
+  config.formatter = :documentation # :progress, :html, :textmate
 end
